e502b071803572d4088c7dc990cf9c236d79dfc7
grace04 <36498022+grace04@users.noreply.github.com>
2018-02-16 15:15:52 -0800

    day 1 java files

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,15 +1,37 @@
 package section2;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot Rob = new Robot();
+		Rob.setRandomPenColor();
+		Rob.setSpeed(10);
+		Rob.miniaturize();
+		Rob.penDown();
+		Rob.setPenWidth(3);
+		for (int i = 0; i < 360; i++) {
+			Rob.move(3);
+			Rob.turn(1);
+		}
+		for (int i = 0; i < 360; i++) {
+			Rob.move(3);
+			Rob.turn(-1);
+		}
+		Rob.penUp();
+		Rob.turn(90);
+		Rob.move(180);
+		Rob.penDown();
+		Rob.setPenWidth(10);
+		Rob.move(3);
+		Rob.penUp();
+		Rob.turn(180);
+		Rob.move(363);
+		Rob.penDown();
+		Rob.move(3);
+		Rob.hide();
 	}
 }
---
+++ src/section3/WorldsNicestProgram.java
@@ -0,0 +1,11 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class WorldsNicestProgram {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "How's your life?");
+		JOptionPane.showMessageDialog(null, "I think your name is Kevin.");
+		JOptionPane.showMessageDialog(null, "Nice name tag.");
+	}
+}
